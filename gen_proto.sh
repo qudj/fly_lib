79c405f859090973b67bbb4ae278df5d72d4050f
qudongjie <qudongjie@bituniverse.org>
2021-12-07 15:15:08 +0800

    change

--- gen_proto.sh
+++ gen_proto.sh
@@ -2,3 +2,7 @@
 protoc --gogofaster_out=plugins=grpc:. --proto_path=/Users/qudongjie/go/src/github.com/qudj/fly_proto fcc.proto
 mkdir -p models/proto/fcc_serv
 mv fcc.pb.go models/proto/fcc_serv/
+
+protoc --gogofaster_out=plugins=grpc:. --proto_path=/Users/qudongjie/go/src/github.com/qudj/fly_proto fly_starling.proto
+mkdir -p models/proto/fly_starling_serv
+mv fly_starling.pb.go models/proto/fly_starling_serv/
